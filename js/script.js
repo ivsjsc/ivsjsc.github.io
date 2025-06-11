896d4ae5fa56616c2213db6038df340870edf232
NGUYỄN MINH TRIẾT <167825527+ivsjsc@users.noreply.github.com>
2025-06-12 04:39:20 +0700

    commit github push and sync

--- js/script.js
+++ js/script.js
@@ -5,26 +5,5 @@ window.debounce = function(func, delay) {
         clearTimeout(debounceTimer);
         debounceTimer = setTimeout(() => func.apply(context, args), delay);
     };
-}
-// stars.js
-const starBackground = document.getElementById('star-background');
-const numberOfStars = 200;
-
-function createStar() {
-    const star = document.createElement('div');
-    star.className = 'star';
-    star.style.left = Math.random() * 100 + 'vw';
-    star.style.top = Math.random() * 100 + 'vh';
-    const size = Math.random() * 2 + 1;
-    star.style.width = size + 'px';
-    star.style.height = size + 'px';
-    star.style.animationDuration = Math.random() * 3 + 2 + 's';
-    star.style.animationDelay = Math.random() * numberOfStars / 50 + 's';
-    starBackground.appendChild(star);
-}
-
-window.onload = function () {
-    for (let i = 0; i < numberOfStars; i++) {
-        createStar();
-    }
+    
 };
